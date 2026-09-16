5fec3e34d6a80717868c301d7ef0b8da01d3041a
Shivam Kumar <2k25cse2511925@gmail.com>
2026-09-16 22:47:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.99%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long start=0;
+        long long end =x;
+        long long ans=0;
+     while(start<=end){
+                long long mid=(start+end)/2;
+                if(mid*mid==x) return mid;
+                else if(mid*mid<x){
+                    ans=mid;
+                    start=mid+1;
+                }
+                else{
+                end=mid-1;
+                }
+        }
+        return ans;
+    }
+};
